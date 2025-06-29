1513ba65eeec31e1864125a00760c00f02902e3c
Elhoussaine <144055760+GANGOUCH1@users.noreply.github.com>
2025-06-29 23:49:22 +0100

    Update data_runner.py

--- data_runner.py
+++ data_runner.py
@@ -6,7 +6,7 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
-
+print(os.getcwd())
 class GeneralTSFDataset(Dataset):
     def __init__(self, hist_len, pred_len, variable, time_feature):
         self.hist_len = hist_len
